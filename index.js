361197ed2ad5b82e2267e7d653bbfcf069825e7b
Luke Song <hyeonminsong7@gmail.com>
2024-01-11 23:08:52 -0800

    :lipstick: Added html and css solution, :recycle: refactored, :iphone, Added Mobile Responsiveness

---
+++ index.js
@@ -0,0 +1,15 @@
+var button = document.getElementById("menu");
+var mobilenav = document.getElementById("mobileNav");
+
+mobilenav.style.display = "none";
+button.style.opacity = 1;
+
+button.onclick = function () {
+  if (mobilenav.style.display == "none") {
+    mobilenav.style.display = "block";
+    button.style.opacity = 0.5;
+  } else {
+    mobilenav.style.display = "none";
+    button.style.opacity = 1;
+  }
+};
